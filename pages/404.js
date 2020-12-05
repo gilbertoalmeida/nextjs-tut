31ac67c0ec24e87986e0d66b1e1b712a8d5e7638
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-12-05 22:37:22 +0100

    full basic nextjs tut for deployment on vercel

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+  return <h1>4444 _ 4444 // Page Not Found</h1>
+}
---
+++ pages/index.js
@@ -0,0 +1,74 @@
+// import { useState, useEffect } from "react"
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/Layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import { fetchFact } from "../Utils/fetchFact"
+import DatePretty from '../components/date'
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  const fact = await fetchFact()
+  return {
+    props: {
+      allPostsData,
+      fact
+    }
+  }
+}
+
+export default function Home({ allPostsData, fact }) {
+  // const [fact, setfact] = useState({})
+
+  // const fetchingFact = async () => {
+  //   const newfact = await fetchFact()
+  //   setfact(newfact)
+  // }
+
+  // useEffect(() => {
+  //   fetchingFact()
+  // }, [])
+
+
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+        <h3>
+          Read{" "}
+          <Link href="posts/first-post">First post</Link>
+        </h3>
+
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <DatePretty dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+        <p>Joke of the day:</p>
+        <p>{fact.text}</p>
+
+      </section>
+    </Layout>
+  )
+}
